a9ecdd5bab74e7ec529826be8e9261308584599e
Rocky <rasel.rocky@franklincovey.com>
2023-04-15 16:20:51 +0800

    Mapped to boilerplate

--- testbench.v
+++ testbench.v
@@ -1,27 +1,11 @@
-module ledmodule_tb();
-reg t_clk, t_rst;
-wire led;
+module NAME_tb();
+//definitions
 
-ledmodule ledmodule_inst(
-    .led(led),
-    .clk(t_clk),
-    .rst(t_rst)
+NAME NAME_inst(
+//map
 );
 
 initial begin
-    t_clk = 0; t_rst = 0;
-    #5 t_clk = 1; t_rst = 0;
-    #5 t_clk = 0; t_rst = 0;
-    #5 t_clk = 1; t_rst = 0;
-    #5 t_clk = 0; t_rst = 0;
-    #5 t_clk = 1; t_rst = 0;
-    #5 t_clk = 0; t_rst = 0;
-    #5 t_clk = 1; t_rst = 0;
-    #5 t_clk = 0; t_rst = 0;
-    #5 t_clk = 1; t_rst = 0;
-    #5 t_clk = 0; t_rst = 0;
-    #5 t_clk = 1; t_rst = 0;
-    #5 t_clk = 0; t_rst = 0;
-    #5 t_clk = 1; t_rst = 0;
+//process
 end
 endmodule
